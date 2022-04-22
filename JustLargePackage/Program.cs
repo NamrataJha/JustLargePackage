02f98680efcf476a9b8c6b16d32ae74b49a554f7
namratajha <namratajha@github.com>
2022-04-22 12:52:06 +0530

    change csproj

--- JustLargePackage/Program.cs
+++ JustLargePackage/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace JustLargePackage
+{
+    public class Class1
+    {
+        public static void Main(){
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
